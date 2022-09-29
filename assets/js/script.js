94d275a39361a69406efc94e0b910a983b2b6fe6
Willian-code <willianrosendodelima@outlook.com>
2022-09-29 12:23:28 -0300

    change fire colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,7 @@ let		fireWidth = 60;
 let		fireHeigth = 40;
 let		debug = false;
 let		direction = 0;
-const fireColorsPalette = [
-	{"r":7,"g":7,"b":7}, {"r":31,"g":7,"b":7},
-	{"r":47,"g":15,"b":7}, {"r":71,"g":15,"b":7},
-	{"r":87,"g":23,"b":7}, {"r":103,"g":31,"b":7},
-	{"r":119,"g":31,"b":7}, {"r":143,"g":39,"b":7},
-	{"r":159,"g":47,"b":7}, {"r":175,"g":63,"b":7},
-	{"r":191,"g":71,"b":7}, {"r":199,"g":71,"b":7},
-	{"r":223,"g":79,"b":7}, {"r":223,"g":87,"b":7},
-	{"r":223,"g":87,"b":7}, {"r":215,"g":95,"b":7},
-	{"r":215,"g":95,"b":7}, {"r":215,"g":103,"b":15},
-	{"r":207,"g":111,"b":15}, {"r":207,"g":119,"b":15},
-	{"r":207,"g":127,"b":15}, {"r":207,"g":135,"b":23},
-	{"r":199,"g":135,"b":23}, {"r":199,"g":143,"b":23},
-	{"r":199,"g":151,"b":31}, {"r":191,"g":159,"b":31},
-	{"r":191,"g":159,"b":31}, {"r":191,"g":167,"b":39},
-	{"r":191,"g":167,"b":39}, {"r":191,"g":175,"b":47},
-	{"r":183,"g":175,"b":47}, {"r":183,"g":183,"b":47},
-	{"r":183,"g":183,"b":55}, {"r":207,"g":207,"b":111},
-	{"r":223,"g":223,"b":159}, {"r":239,"g":239,"b":199},
-	{"r":255,"g":255,"b":255}
-]
+const lightnessColorsPalette = [0, 5.40, 8.10, 10.80, 13.50, 16.20, 18.90, 21.60, 24.30, 27, 29.70, 32.40, 35.10, 37.80, 40.50, 43.20, 45.90, 48.60, 51.30, 54, 56.70, 59.40, 62.10, 64.80, 67.50, 70.20, 72.90, 75.60, 78.30, 81, 83.70, 86.40, 89.10, 91.80, 94.50, 97.20, 100]
 
 function createFireDataStructure() {
 	const	numberOfPixels = (fireWidth * fireHeigth);
@@ -42,6 +22,14 @@ function createFireSource() {
 	}
 }
 
+function toHslString(fireIntensity) {
+	const	hue = Number(document.getElementById('hueInput').value)
+	let		lightness = lightnessColorsPalette[fireIntensity]
+
+	lightness += "%";
+	return `hsl(${hue}, 100%, ${lightness})`;
+}
+
 function renderFire() {
 	let	html = '<table cellpadding=0 cellspacing=0>';
 
@@ -57,9 +45,7 @@ function renderFire() {
 				html += fireIntensity;
 				html += '</td>';
 			} else {
-				const	color = fireColorsPalette[fireIntensity];
-				const	colorString = `${color.r}, ${color.g}, ${color.b}`;
-				html += `<td class="pixel" style="background-color: rgb(${colorString})"`;
+				html += `<td class="pixel" style="background-color: ${toHslString(fireIntensity)}"`;
 				html += `</td>`;
 			}
 		}
